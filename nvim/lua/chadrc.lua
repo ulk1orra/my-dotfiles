1e5b50f6dbffc2d572738b08a1f8945b1d97f117
ulk1orra <mrfedotka01@gmail.com>
2026-02-23 19:45:53 +0300

    New image wallpaper

--- nvim/lua/chadrc.lua
+++ nvim/lua/chadrc.lua
@@ -6,19 +6,19 @@
 local M = {}
 
 M.base46 = {
-	theme = "gruvchad",
+	theme = "kanagawa-dragon",
 
-	-- hl_override = {
-	-- 	Comment = { italic = true },
-	-- 	["@comment"] = { italic = true },
-	-- },
+	hl_override = {
+		Comment = { italic = true },
+		["@comment"] = { italic = true },
+	},
 }
 
--- M.nvdash = { load_on_startup = true }
--- M.ui = {
---       tabufline = {
---          lazyload = false
---      }
--- }
+M.nvdash = { load_on_startup = true }
+M.ui = {
+      tabufline = {
+         lazyload = false
+     }
+}
 
 return M
